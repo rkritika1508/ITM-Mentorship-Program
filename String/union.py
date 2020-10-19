de08250ed8294a03d3762c40d1b808700fc40413
CodArtist <72666944+CodArtist@users.noreply.github.com>
2020-10-19 13:34:36 +0530

    Update union.py

--- String/union.py
+++ String/union.py
@@ -1,18 +1,17 @@
-def union(list1,list2):
-    le=len(list1)
-    list1[le:le]=list2
-    list4=[]
-    list3=list(dict.fromkeys(list1))
-    for i in range(len(list3)):
-    	list4.append(int(list3[i]))
-    list4.sort()
-    for i in range(len(list4)):
-    	print(list4[i],end=" ")
+def union(A,B):
+    length=len(A)
+    A[length:length]=B
+    Result=[]
+    Temp=list(dict.fromkeys(A))
+    for i in range(len(Temp)):
+    	Result.append(int(Temp[i]))
+    Result.sort()
+    for i in range(len(Result)):
+    	print(Result[i],end=" ")
     
         
 M,N=input().split()
-M=int(M)
-N=int(N)
-l1= list(map(int, input().strip().split()))[:M]
-l2=list(map(int, input().strip().split()))[:N]
-union(l1,l2)
+M,N=int(M),int(N)
+A= list(map(int, input().strip().split()))[:M]
+B=list(map(int, input().strip().split()))[:N]
+union(A,B)
