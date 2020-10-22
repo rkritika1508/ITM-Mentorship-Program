582c8867a54915543f14e6f9e64bef58e40ae7fc
Anuradha0501 <71958796+Anuradha0501@users.noreply.github.com>
2020-10-22 19:06:49 +0530

    Update ReverseDoublyLinkedList.cpp

--- Contest - 1 - October 18 2020/cpp contest codes/ReverseDoublyLinkedList.cpp
+++ Contest - 1 - October 18 2020/cpp contest codes/ReverseDoublyLinkedList.cpp
@@ -11,18 +11,19 @@
  *
  */
 DoublyLinkedListNode* reverse(DoublyLinkedListNode* head) {
-    DoublyLinkedListNode *temp = head;
-    DoublyLinkedListNode *current = head;
-    while(current != NULL)
+    DoublyLinkedListNode *temp = head;  // here pointer temp will point head of linked list
+    DoublyLinkedListNode *current = head;     //here we will copy head's address in current pointer 
+    //here we use two pointers for storing head's address so values of head will not be lost
+    while(current != NULL)  // ifcurrent is not null swap the next and prev node
     {
         temp = current->prev;
         current->prev = current->next;
         current->next = temp;
         current = current->prev;
-    }
-    if(temp!=NULL)
+    } //after while loop our linked list will reversed
+    if(temp!=NULL)  // if temp is not null assign prev to head 
     {
         head = temp->prev;
     }
-    return head;
+    return head;   //this will return head to our main program
 }
