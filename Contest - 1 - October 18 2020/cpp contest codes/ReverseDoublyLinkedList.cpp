608930e5f1ccd89efbce557b619fb8b5a001e37b
Anuradha0501 <71958796+Anuradha0501@users.noreply.github.com>
2020-10-22 18:48:54 +0530

    Create ReverseDoublyLinkedList.cpp

---
+++ Contest - 1 - October 18 2020/cpp contest codes/ReverseDoublyLinkedList.cpp
@@ -0,0 +1,28 @@
+// Complete the reverse function below.
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+DoublyLinkedListNode* reverse(DoublyLinkedListNode* head) {
+    DoublyLinkedListNode *temp = head;
+    DoublyLinkedListNode *current = head;
+    while(current != NULL)
+    {
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+        current = current->prev;
+    }
+    if(temp!=NULL)
+    {
+        head = temp->prev;
+    }
+    return head;
+}
