a66e5ca7d41e41e792ca1a40e2c43c1eb518a845
shivani278 <71933208+shivani278@users.noreply.github.com>
2020-10-23 19:49:13 +0530

    Assignment_ques

---
+++ Week 2/Assingnment_question/amazing_subarrays.py
@@ -0,0 +1,12 @@
+def amazing(s):
+    vowels=["a",'e','i','o','u','A','E','O','U',"I"]
+    count=0
+    for i in range(len(s)):
+        if s[i] in vowels:
+           count+=len(string)-i
+    return(count%10003)
+
+if __name__=="__main__":
+    string=input()
+    x=amazing(string)
+    print(x)
