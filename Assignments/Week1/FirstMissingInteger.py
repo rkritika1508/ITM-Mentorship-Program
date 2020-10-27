3682b38c4205d65463355395e43f35db05a3dd3d
CodArtist <72666944+CodArtist@users.noreply.github.com>
2020-10-27 23:51:51 +0530

    Update FirstMissingInteger.py

--- Assignments/Week1/FirstMissingInteger.py
+++ Assignments/Week1/FirstMissingInteger.py
@@ -1,8 +1,24 @@
-class Solution:
+ class Solution:
     def firstMissingPositive(self, A):
-        hashmap=[0]*len(A)
-        
+        A.sort()
+        counter=0
+        t=1
         for i in range(len(A)):
-            if A[i]==0:
-                return i+1
-        return i+2    
+            if A[i]>0 and i<len(A)-1:
+                if t:
+                    counter=counter+1
+                if counter==1:
+                    t=0
+                    counter=0
+                    if A[i]!=1:
+                        return 1
+                
+                if A[i+1]-A[i]!=1 and A[i]!=A[i+1]:
+                    return A[i]+1
+        if i!=0:
+            return A[i]+1
+            
+        if A[i]!=1:
+            return 1
+        else:
+            return 2
