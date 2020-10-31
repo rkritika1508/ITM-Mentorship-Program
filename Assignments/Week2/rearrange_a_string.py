1d02f1e9d38c6939cf1d43c9b4cfcc30b975610a
shivani278 <71933208+shivani278@users.noreply.github.com>
2020-10-31 20:58:30 +0530

    added captions

--- Assignments/Week2/rearrange_a_string.py
+++ Assignments/Week2/rearrange_a_string.py
@@ -1,19 +1,19 @@
 def rearrange(string):
-    count2=0
-    count1=0
+    sum_of_digits=0
+    counter=0
     for i in string:
         if i.isdigit():
-            count2+=int(i)
-            count1+=1
-
-    a1=sorted(string)
-    for i in range(count1):
-        a1.pop(0)
-    return(a1,count2)
+            sum_of_digits+=int(i) #increase the sum by adding digits present in the string
+            counter+=1 #counting the no of digits
+    
+    sorted_string=sorted(string)
+    for i in range(counter):
+        sorted_string.pop(0)  #removing the digits
+    return(sorted_string,sum_of_digits)
     
 for _ in range(int(input())):
-    a=input()
-    x,y=rearrange(a)
-    for i in x:
+    string=input()
+    str,digit=rearrange(string)
+    for i in str:
         print(i,end="")
-    print(y)
+    print(digit)
